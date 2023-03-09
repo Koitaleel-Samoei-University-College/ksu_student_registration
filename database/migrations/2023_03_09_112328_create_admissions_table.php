496dfd5f02a9456719592e061bef165f4297c7ca
Jesse Songok <jessesongok@gmail.com>
2023-03-09 14:27:09 +0300

    Admissions Table

---
+++ database/migrations/2023_03_09_112328_create_admissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('admission_number');
+            $table->unsignedBigInteger('student_id');
+            $table->boolean('status');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admissions');
+    }
+};

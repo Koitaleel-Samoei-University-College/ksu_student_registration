c44c6bd3b0a1bce3e21529e8675c08066f819076
Jesse Songok <jessesongok@gmail.com>
2023-03-09 12:54:20 +0300

    Added Icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-3">
             <ul class="list-group">
                 <a href="{{route('file-import')}}" class="list-group-item"><i class="bi bi-cloud-upload"></i> Upload KUCCPS LIST</a>
-                <li class="list-group-item">Admission Letters</li>
+                <li class="list-group-item"> <i class="bi bi-file-earmark-pdf"></i> Admission Letters</li>
                 <a href="{{route('students')}}" class="list-group-item"> <i class="bi bi-card-list"></i> Student List</a>
             </ul>
         </div>

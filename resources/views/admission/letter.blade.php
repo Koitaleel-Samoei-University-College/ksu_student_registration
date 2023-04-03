f974131facb2eaab0c4e05d9b5d4fe40cbfb1dcd
Jesse Songok <jessesongok@gmail.com>
2023-04-03 11:06:10 +0300

    generate letter

--- resources/views/admission/letter.blade.php
+++ resources/views/admission/letter.blade.php
@@ -1,15 +1,31 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <img src="{{asset('logo.png')}}" width="60" height="50">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+
+            <img class="rounded mx-auto d-block" src="{{asset('logo.png')}}" width="100" height="100">
             <strong>
                <div class="row justify-content-center"> KOITALEEL SAMOEI UNIVERSITY COLLEGE</div>
                <div class="row justify-content-center"> (A Constituent College of the University of Nairobi)</div>
                <div class="row justify-content-center"> OFFICE OF THE ACADEMIC REGISTRAR</div>
             </strong>
-        </div>
+
         <p>
         <strong>
             <div>P.O. Box 5 - 30307,</div>
@@ -81,5 +97,7 @@
 
         </p>
     </div>
-@endsection
 
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>

91fec6101c01460ade7b99aa62f5139d23f5767c
Jesse Songok <jessesongok@gmail.com>
2023-08-02 11:08:19 +0300

    Refactor: Admission Letter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         </style>
     </head>
     <body >
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-200  py-5 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-200  py-2 sm:pt-0">
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8  sm:pt-0">
